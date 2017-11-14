30bea3ee21ebf7655301a1589ca2dd3469d5cbb3
Ayrat <iqtc@yandex.ru>
2017-11-14 19:30:04 +0300

    Что то добавили

--- src/main/java/ru/bugmakers/controller/Controller.java
+++ src/main/java/ru/bugmakers/controller/Controller.java
@@ -1,7 +1,21 @@
 package ru.bugmakers.controller;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by ivan
  */
+@RestController
+@RequestMapping("/")
 public class Controller {
+
+    @RequestMapping(method = RequestMethod.GET, value = "test")
+    @ResponseBody
+    public String testRest(){
+        return null;
+    }
 }
