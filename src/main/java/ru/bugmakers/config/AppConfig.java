b94c792395d1d4a71d88dca5363641f36aa9e092
ivan <ikolpakoff@gmail.com>
2017-12-15 19:00:42 +0300

    Доработки для запуска приложения

--- src/main/java/ru/bugmakers/config/AppConfig.java
+++ src/main/java/ru/bugmakers/config/AppConfig.java
@@ -2,6 +2,7 @@ package ru.bugmakers.config;
 
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -11,8 +12,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 @ComponentScan(value = "ru.bugmakers", excludeFilters = @Filter(Configuration.class))
-//@Import(PersistConfig.class)
-//@Import(SecurityConfig.class)
+@EnableJpaRepositories(basePackages = "ru.bugmakers.repository", entityManagerFactoryRef = "emf")
+@Import({SecurityConfig.class, PersistConfig.class})
 public class AppConfig {
 
 
